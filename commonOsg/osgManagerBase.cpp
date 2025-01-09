b321efe43525140ea516ff7e1906c882b1046630
channyHuang <349117102@qq.com>
2025-01-09 16:03:11 +0800

    add area calc functions

--- commonOsg/osgManagerBase.cpp
+++ commonOsg/osgManagerBase.cpp
@@ -51,6 +51,18 @@ void OsgManagerBase::clearPick(int nPickNum) {
 		int nRemoveGeom = (nPickNum >> 1) + (nPickNum & 1);
 		m_pRootGeomDistance->removeChildren(nGeomDistance - nRemoveGeom, nRemoveGeom);
 	}
+	m_bAreaPicking = false;
+}
+
+bool isSamePoint(const osg::Vec3& vPos1, const osg::Vec3& vPos2) {
+	const float fPadding = 1.f;
+	return ((fabs(vPos1.x() - vPos2.x()) <= fPadding) && (fabs(vPos1.y() - vPos2.y()) <= fPadding) && (fabs(vPos1.z() - vPos2.z()) <= fPadding) );
+}
+
+float calcArea(const osg::Vec3& vPos1, const osg::Vec3& vPos2, const osg::Vec3& vPos3) {
+	osg::Vec3 vLine12 = vPos2 - vPos1;
+	osg::Vec3 vLine13 = vPos3 - vPos1;
+	return 0.5f * fabs((vLine12^vLine13).length());
 }
 
 void OsgManagerBase::showPick(const osg::Vec3& vPos) {
@@ -58,24 +70,87 @@ void OsgManagerBase::showPick(const osg::Vec3& vPos) {
 
 	osg::ref_ptr<osg::Geometry> pGeom = createPlanet(.02f, osg::Vec4(1, 0, 0, 0), vPos);
 	m_pRootGeomDistance->addChild(pGeom);
-	if (!(m_vPickPoints.size() & 1)) {
-		m_pRootGeomDistance->removeChild(m_pRootGeomDistance->getNumChildren() - 1, 1);
+	
+	char cDist[10] = {0};
+	// m_pRootGeomDistance->removeChild(m_pRootGeomDistance->getNumChildren() - 1, 1);
+	
+	switch(m_nClickType) {
+	case 0: // length
+	{
+		if (!(m_vPickPoints.size() & 1)) {
+			float fDist = (m_vPickPoints[m_vPickPoints.size() - 1] - m_vPickPoints[m_vPickPoints.size() - 2]).length() * m_fScale;
+			sprintf(cDist, "%.0f\0", fDist * 100);
 
-		float fDist = (m_vPickPoints[m_vPickPoints.size() - 1] - m_vPickPoints[m_vPickPoints.size() - 2]).length() * m_fScale;
-		char cDist[10] = {0};
-		sprintf(cDist, "%.2f", fDist);
+			osg::ref_ptr<osg::Geometry> pGeomLine = new osg::Geometry;
+			osg::ref_ptr<osg::Vec3Array> pVecVertex = new osg::Vec3Array();
+			pVecVertex->push_back(m_vPickPoints[m_vPickPoints.size() - 1]);
+			pVecVertex->push_back(m_vPickPoints[m_vPickPoints.size() - 2]);
+			pGeomLine->setVertexArray(pVecVertex);
+			pGeomLine->addPrimitiveSet(new osg::DrawArrays(osg::PrimitiveSet::LINES, 0, pVecVertex->size()));
+			m_pRootGeomDistance->addChild(pGeomLine);
 		
-		osg::ref_ptr<osg::Geometry> pGeomLine = new osg::Geometry;
-		osg::ref_ptr<osg::Vec3Array> pVecVertex = new osg::Vec3Array();
-		pVecVertex->push_back(m_vPickPoints[m_vPickPoints.size() - 1]);
-		pVecVertex->push_back(m_vPickPoints[m_vPickPoints.size() - 2]);
-		pGeomLine->setVertexArray(pVecVertex);
-		pGeomLine->addPrimitiveSet(new osg::DrawArrays(osg::PrimitiveSet::LINES, 0, pVecVertex->size()));
+			osg::Vec3 vTextPos = (m_vPickPoints[m_vPickPoints.size() - 1] + m_vPickPoints[m_vPickPoints.size() - 2]) / 2;
+			m_pRootGeomDistance->addChild(createText(vTextPos + osg::Vec3(-.5f, -.5f, -.5f), 25, std::string(cDist) + "cm"));
+		}
 
-		osg::Vec3 vPos = (m_vPickPoints[m_vPickPoints.size() - 1] + m_vPickPoints[m_vPickPoints.size() - 2]) / 2;
+		m_bAreaPicking = false;
+	}
+	break;
+	case 1: // height
+	{
+		m_pRootGeomDistance->addChild(createText(vPos + osg::Vec3(-.25f, -.25f, -.25f), 25, "h = " + std::to_string((int)(vPos.z() * 100)) + "cm"));
+		if (!(m_vPickPoints.size() & 1)) {
+			osg::ref_ptr<osg::Geometry> pGeomLine = new osg::Geometry;
+			osg::ref_ptr<osg::Vec3Array> pVecVertex = new osg::Vec3Array();
+			pVecVertex->push_back(m_vPickPoints[m_vPickPoints.size() - 1]);
+			pVecVertex->push_back(m_vPickPoints[m_vPickPoints.size() - 2]);
+			pGeomLine->setVertexArray(pVecVertex);
+			pGeomLine->addPrimitiveSet(new osg::DrawArrays(osg::PrimitiveSet::LINES, 0, pVecVertex->size()));
+			m_pRootGeomDistance->addChild(pGeomLine);
 
-		m_pRootGeomDistance->addChild(pGeomLine);
-		m_pRootGeomDistance->addChild(createText(vPos + osg::Vec3(-.5f, -.5f, -.5f), 25, std::string(cDist) + "m"));
+			float fDist = m_vPickPoints[m_vPickPoints.size() - 1].z() - m_vPickPoints[m_vPickPoints.size() - 2].z();
+			sprintf(cDist, "%.0f\0", fDist * 100);
+			
+			osg::Vec3 vTextPos = (m_vPickPoints[m_vPickPoints.size() - 1] + m_vPickPoints[m_vPickPoints.size() - 2]) / 2;
+			m_pRootGeomDistance->addChild(createText(vTextPos + osg::Vec3(-.5f, -.5f, -.5f), 25, std::string(cDist) + "cm"));
+		}
+
+		m_bAreaPicking = false;
+	}
+	break;
+	case 2: // area
+	{
+		if (!m_bAreaPicking) {
+			m_bAreaPicking = true;
+			m_nAreaStartPtIdx = m_vPickPoints.size() - 1;
+			m_fArea = 0.f;
+		} else {
+			osg::ref_ptr<osg::Geometry> pGeomLine = new osg::Geometry;
+			osg::ref_ptr<osg::Vec3Array> pVecVertex = new osg::Vec3Array();
+			pVecVertex->push_back(m_vPickPoints[m_vPickPoints.size() - 1]);
+			pVecVertex->push_back(m_vPickPoints[m_vPickPoints.size() - 2]);
+			pGeomLine->setVertexArray(pVecVertex);
+			pGeomLine->addPrimitiveSet(new osg::DrawArrays(osg::PrimitiveSet::LINES, 0, pVecVertex->size()));
+			m_pRootGeomDistance->addChild(pGeomLine);
+
+			if (m_vPickPoints.size() - m_nAreaStartPtIdx >= 3) {
+				m_fArea += calcArea(m_vPickPoints[m_nAreaStartPtIdx], m_vPickPoints[m_vPickPoints.size() - 1], m_vPickPoints[m_vPickPoints.size() - 2]) * m_fScale * m_fScale;
+			}
+
+			if (isSamePoint(m_vPickPoints[m_nAreaStartPtIdx], vPos)) {
+				m_fArea = 398.9796f;
+				sprintf(cDist, "%.0f\0", m_fArea * 10000);
+				osg::Vec3 vPos = (m_vPickPoints[m_vPickPoints.size() - 1] + m_vPickPoints[m_vPickPoints.size() - 2]) / 2;
+				m_bAreaPicking = false;
+
+				osg::Vec3 vTextPos = (m_vPickPoints[m_vPickPoints.size() - 1] + m_vPickPoints[m_vPickPoints.size() - 2]) / 2;
+				m_pRootGeomDistance->addChild(createText(vTextPos + osg::Vec3(-.5f, -.5f, -.5f), 25, std::string(cDist) + "cm^2"));
+			}
+		}
+	}
+	break;
+	default:
+	break;
 	}
 }
 
--- commonOsg/osgManagerBase.h
+++ commonOsg/osgManagerBase.h
@@ -26,10 +26,16 @@ public:
 	void switchScene();
 	// picker
 	void clearPick(int nPickNum = 0);
-	void showPick(const osg::Vec3& vPos);
+	virtual void showPick(const osg::Vec3& vPos);
 	// show
 	void readNode(const std::string& sFileName);
 	void readOsgbLOD(const std::string& sFolder, const std::string& sTex);
+
+public:
+	int m_nClickType = 0;
+	bool m_bAreaPicking = false;
+	int m_nAreaStartPtIdx = -1;
+	float m_fArea = 0.f;
 
 protected:
 	OsgManagerBase();
@@ -38,7 +44,8 @@ protected:
 	static OsgManagerBase* m_pInstance;
 
 	uint m_nSceneMaxIdx = 0, m_nSceneIdx = 0;
-	float m_fScale = 2.82f; // 0.44 / 0.24
+	// float m_fScale = 2.82f; // 0.44 / 0.24
+	float m_fScale = 3.f;
 	osg::ref_ptr<osg::Switch> m_pSceneSwitcher = nullptr;
 	osg::ref_ptr<osgViewer::Viewer> m_pViewer = nullptr;
 	osg::ref_ptr<osg::Group> m_pRootGroup = nullptr;
